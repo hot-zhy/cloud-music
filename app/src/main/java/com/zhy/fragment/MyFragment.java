ba77873d3b7963542ec4f090e372f1055c13f46e
zhy <2038290448@qq.com>
2022-11-26 01:28:23 +0800

    实现我的界面点击功能

--- app/src/main/java/com/zhy/fragment/MyFragment.java
+++ app/src/main/java/com/zhy/fragment/MyFragment.java
@@ -1,6 +1,9 @@
 package com.zhy.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.zhy.zhycloudmusic.BaseViewModeActivity;
 import com.zhy.zhycloudmusic.databinding.FragmentDiscoverBinding;
@@ -10,12 +13,48 @@ import com.zhy.zhycloudmusic.databinding.FragmentMyBinding;
  * 我的界面
  */
 public class MyFragment extends BaseViewModelFragment<FragmentMyBinding> {
-    public static MyFragment newInstance() {
+    @Override
+    protected void initListeners() {
+        super.initListeners();
+        //用户点击
+        View.OnClickListener userClick = view -> {
 
-        Bundle args = new Bundle();
+        };
+        /**
+         * 点击图标
+         */
+        binding.icon.setOnClickListener(userClick);
+        /**
+         * 点击昵称
+         */
+        binding.nickname.setOnClickListener(userClick);
+        //关于点击
+        binding.about.setOnClickListener(v -> {
+            Intent intent=new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            Uri uri=Uri.parse("https://hot-zhy.github.io/");
+            intent.setData(uri);
+            startActivity(intent);
+        });
+        //设置点击
+        binding.setting.setOnClickListener(v -> {
 
-        MyFragment fragment = new MyFragment();
-        fragment.setArguments(args);
-        return fragment;
+        });
+        //客服点击
+        binding.service.setOnClickListener(v -> {
+            Intent intent =new Intent(Intent.ACTION_DIAL,Uri.parse("tel:19933153268"));
+            startActivity(intent);
+        });
+
     }
+
+        public static MyFragment newInstance () {
+
+            Bundle args = new Bundle();
+
+            MyFragment fragment = new MyFragment();
+            fragment.setArguments(args);
+            return fragment;
+        }
+
 }
